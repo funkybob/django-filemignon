73a062d932d388df4094a41f664e647a1c69c311
Curtis Maloney <curtis@tinbrain.net>
2013-09-28 22:34:09 +1000

    Add dir_list view

--- filemignon/views.py
+++ filemignon/views.py
@@ -1,8 +1,31 @@
-from django.http import HttpResponse
+
+from django.http import HttpResponse, HttpResponseNotAllowed
 from django.conf import settings
-from django.http import HttpResponseNotAllowed
+
+import os.path
 
 storage = settings.FILEMIGNON_STORAGE
+
+def dir_list(request, path):
+    dirs, files = storage.listdir(path)
+
+    return JsonResponse({
+        'directories': [
+            {
+                'name': name,
+            }
+            for name in dirs
+        ],
+        'files': [
+            {
+                'name': name,
+                # 'size': 
+                'created': storage.created_time(os.path.join(path, name)),
+                'modified': storage.modified_time(os.path.join(path, name)),
+            }
+            for name in files
+        ],
+    })
 
 def delete(request, filename):
     try:
@@ -10,3 +33,4 @@ def delete(request, filename):
         return HttpResponse(status_code=204)
     except NotImplementedError as ex:
         return HttpResponseNotAllowed(str(ex))
+
